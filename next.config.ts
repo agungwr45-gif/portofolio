cb4f932b3344275870c8f5daa580df062865c6e8
Agung Wahyu Riyadi <agungwr45@gmail.com>
2026-04-20 14:39:50 +0700

    chore: configuration update for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portofolio',
-  assetPrefix: '/portofolio/',
   trailingSlash: true,
   images: {
     unoptimized: true,
